b6763e83f72a4e16c88973016d1b201099bcdd5e
iqbalatma <iqbalatma@gmail.com>
2023-01-14 16:09:21 +0700

    change layout on email verification

--- resources/views/auth/verification.blade.php
+++ resources/views/auth/verification.blade.php
@@ -1,27 +1,27 @@
-<x-app-layout :title="$title">
-  <div class="card mb-4">
-    <div class="card-header">
-      <i class="fa-solid fa-file-invoice-dollar"></i>
-      {{ $cardTitle }}
+<x-dashboard.layout :title="$title">
+    <div class="card mb-4">
+        <div class="card-header">
+            <i class="fa-solid fa-file-invoice-dollar"></i>
+            {{ $cardTitle }}
+        </div>
+        <div class="card-body">
+            @if (session('resent'))
+            <div class="alert alert-success" role="alert">
+                A fresh verification link has been sent to your email address.
+            </div>
+            @endif
+
+            Before proceeding, please check your email for a verification link. If you did not receive the email,
+            <form action="{{ route('verification.resend') }}" method="POST" class="d-inline">
+                @csrf
+                <button type="submit" class="d-inline btn btn-link p-0">
+                    click here to request another
+                </button>.
+            </form>
+        </div>
     </div>
-    <div class="card-body">
-      @if (session('resent'))
-      <div class="alert alert-success" role="alert">
-        A fresh verification link has been sent to your email address.
-      </div>
-      @endif
-
-      Before proceeding, please check your email for a verification link. If you did not receive the email,
-      <form action="{{ route('verification.resend') }}" method="POST" class="d-inline">
-        @csrf
-        <button type="submit" class="d-inline btn btn-link p-0">
-          click here to request another
-        </button>.
-      </form>
-    </div>
-  </div>
 
 
 
 
-</x-app-layout>
+</x-dashboard.layout>
